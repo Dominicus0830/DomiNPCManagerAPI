3338e9610954275de039134cf44b3b69ca1aeb33
Dominicus0830 <100403377+Dominicus0830@users.noreply.github.com>
2024-08-01 23:43:42 +0900

    ds

---
+++ src/main/java/org/domi/events/interfaces/EventsInterface.java
@@ -0,0 +1,13 @@
+package org.domi.events.interfaces;
+
+import org.bukkit.event.EventHandler;
+import org.domi.events.DomiNPCClickEvent;
+import org.domi.events.DomiNPCRightClickEvent;
+
+public interface EventsInterface {
+    @EventHandler
+    void onClickDomiNPC(DomiNPCClickEvent event);
+
+    @EventHandler
+    void onRightClickDomiNPC(DomiNPCRightClickEvent event);
+}
